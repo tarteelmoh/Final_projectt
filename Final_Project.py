126cba86d324b66dfd40c78a771290d9878cabd6
tarteelmoh <t421101569@gmail.com>
2023-06-19 17:18:34 +0300

    TODO5

--- Final_Project.py
+++ Final_Project.py
@@ -48,7 +48,8 @@ class Student:
 
 
     # TODO 5 define a method to enroll new course to student courses list
-
+    def enroll_course(self, course):
+        self.courses_list.append(course)
 
     # method to get_student_details as dict
     def get_student_details(self):
