084f8242e9fe4de34c633d2ee6757aff43dbe511
tarteelmoh <t421101569@gmail.com>
2023-06-19 17:17:24 +0300

    TODO3,4

--- Final_Project.py
+++ Final_Project.py
@@ -24,6 +24,7 @@ course = Course(course_name,course_mark)
 
 class Student:
     # TODO 3 define static variable indicates total student count
+    total_student_count = 0
 
     # TODO 4 define a constructor which includes
     # student_id (unique using uuid module)
@@ -31,10 +32,23 @@ class Student:
     # student_age (user input)
     # student_number (user_input)
     # courses_list (List of Course Objects)
-    def __init__(self):
-        pass
+    def __init__(self, student_name, student_age, student_number):
+        self.student_id = str(uuid.uuid4())
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = []
+        Student.total_student_count += 1
+
+    student_name = input('Enter your name :')
+    student_age = int(input('Enter your age :'))
+    student_number = int(input('Enter your number:'))
+    courses_list = course
+    student1 = (student_name,student_age,student_number,course)
+
 
     # TODO 5 define a method to enroll new course to student courses list
+
 
     # method to get_student_details as dict
     def get_student_details(self):
