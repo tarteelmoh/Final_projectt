c5b16fbf6fb2f99ba5eb53d2aca82beac0a87226
tarteelmoh <t421101569@gmail.com>
2023-06-19 17:23:57 +0300

    TODO6

--- Final_Project.py
+++ Final_Project.py
@@ -57,8 +57,11 @@ class Student:
 
     # method to get_student_courses
     def get_student_courses(self):
-        # TODO 6 print student courses with their marks
-        pass
+        for course in self.courses_list:
+            print(f"Course Name: {course.course_name}, Mark: {course.course_mark}")
+
+            # TODO 6 print student courses with their marks
+
 
     # method to get student_average as a value
     def get_student_average(self):
